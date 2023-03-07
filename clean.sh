a7e9491a074bf7f36c06be9103a7b64431dda889
Gie "Max" Vanommeslaeghe <gievanom@hotmail.com>
2023-03-07 11:59:45 +0100

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#This script will CLEAN RakNet, DummyServer and the Streaming client.
+cd 3rdparty/RakNet
+rm -rf build
+cd ..
+cd ..
+#Clean DummyServer
+cd DummyServer
+rm -rf build
+cd ..
+cd ..
+#Clean client
