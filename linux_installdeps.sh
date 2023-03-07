0183544567e944990a658c8110432c443c482189
Gie "Max" Vanommeslaeghe <gievanom@hotmail.com>
2023-03-07 11:59:04 +0100

    Create linux_installdeps.sh

---
+++ linux_installdeps.sh
@@ -0,0 +1,2 @@
+sudo apt-get install build-essential cmake gcc gdb -y
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev libgstrtspserver-1.0-dev -y
